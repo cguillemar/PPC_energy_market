c7e81d80e50295943df5fac54dcf942299872460
cguillemar <45397331+cguillemar@users.noreply.github.com>
2018-12-10 17:55:37 +0100

    Add files via upload

---
+++ Energy_market.py
@@ -0,0 +1,60 @@
+
+class Meteo:
+
+
+    def __init__(self,date,ensoleilement,temperature,vent):
+        self.date=[]
+        self.ensoleilement
+        self.temperature
+        self.vent
+        self.alpha
+
+    
+
+
+
+class Maison:
+
+    def __init__(self,consume,product,etat):
+        self.num
+        self.consume
+        self.product
+        self.etat
+        self.energie
+
+
+class Marche:
+
+    def __init__(self,monPrixInitial):
+        self.prix=[]
+        prix.append(monPrixInitial)
+
+    def __calculPrix__(self):
+        i=0
+        while True:
+            prix.append(gamma*prix[i]+sommeExt)
+            i+=1
+
+
+class EvenExt:
+
+    def __init__(self):
+        self.beta #
+        self.u #apparition d'un evenement (0 ou 1)
+        self.sommeExt
+        self.evenements
+
+
+if __name__=='__main__':
+
+
+
+
+
+
+
+
+
+
+#ma_Meteo=Meteo()
+
