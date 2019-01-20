e222d9ab1ebbc41c0913d65e3b16ec946563c396
cguillemar <45397331+cguillemar@users.noreply.github.com>
2019-01-20 22:31:24 +0100

    Update EnergyMarketVf.py

--- EnergyMarketVf.py
+++ EnergyMarketVf.py
@@ -23,7 +23,7 @@ def meteo(listTemp, N):
     for i in range(N):
         listTemp[i] = tempf[i]
 
-
+#Process Marche
 class Marche(Process):
     def __init__(self, vPrix, tabMarche, listTemp, portefeuille, flagFinishTurn, monJour, synCondition, suiviMaison,
                  tabProduction, nMaison, suiviMaison2):
@@ -42,197 +42,181 @@ class Marche(Process):
 
     def run(self):
         print("--------- Creation marche ---------")
-
+        
+        #creation du process evenement exterrieur et communication par pipes
         parent_conn, child_conn = Pipe()
         pEvenementExt = Process(target=evenementExt, args=(child_conn,))
         pEvenementExt.start()
-
+        
+        # boucle while pour que la simulation fonctionne sur 366 jours
         while self.monJour.value != 365:
-
+               
+            #par pipes: Marche indique a Evenement ext quel jour on est
             parent_conn.send(self.monJour.value)
+            
+            #dayEvent: valeur aleatoire generee par evenement ext qui va influencer dans la formule du prix
             dayEvent = parent_conn.recv();
-
+            
+            #nrj: somme de tout les echanges (achats,ventes) entre Maisons et Marche
             nrj = 0
-
-            # calcul du nouveau prix pour le jour suivant
-            # self.vPrix[self.monJour.value+1]=0.5
-
-            # print("nouveau prix", self.vPrix[self.monJour.value + 1])
-            # print("nous sommes jour", self.monJour.value)
+            
+            #boucle while: fonctionne tant que tout les flags maisons ne sont pas True
             while Marche.__test__(self, self.nMaison) != True:
+                
                 while self.tabMarche.empty() == False:
-                    # print(self.monJour.value)
+                    #recupere et gere les donnees de tabMarche
                     indice, valeur = self.tabMarche.get()
-
-                    # print(indice,valeur,"jour",self.monJour.value)
                     nrj = nrj + valeur
                     threadAchatMarche = threading.Thread(target=Marche.__achat_marche__, args=(self, indice, valeur))
                     threadAchatMarche.start()
                     threadAchatMarche.join()
-                # maj du jour apres avoir verifie que les 10 process maisons ont finie leur tour
-
+            #fin du jour:
+            #maj du prix
             self.vPrix[self.monJour.value + 1] = (0.99 * self.vPrix[self.monJour.value] + Marche.prix(nrj) + dayEvent)
-
+            
+            #ecriture de la valeur du prix et du jour en cours dans Courbes.txt
             fichier = open("Courbes.txt", "a")
             fichier.write(str(self.vPrix[self.monJour.value] + 3.5) + "," + str(self.monJour.value) + "\n")
             fichier.close()
-
-            # time.sleep(0.05)
-
+            
+            #ecriture du jour dans nrj.txt
             fichier = open("nrj.txt", "a")
             fichier.write("Jour : " + str(self.monJour.value + 1) + "\n")
             fichier.close()
-
+            
+            #maj du jour 
             self.monJour.value += 1
-
+            
+            #on vide tabProduction
             if self.tabProduction.empty() == False:
                 x = self.tabProduction.get()
-
-            # print("marche",self.monJour.value)
-            self.synCondition.acquire()
+            
+            self.synCondition.acquire() #comme un semaphore (pas de limite de taille)
+            
+            #on remet tout les flags maisons a False
             for i in range(self.nMaison):
                 self.flagFinishTurn[i] = False
+            #on libere tout les process maisons qui etaient en attente (synCondition.wait())
             self.synCondition.notify_all()
-            self.synCondition.release()
+            self.synCondition.release()#comme un semaphore (pas de limite de taille)
 
             self.suiviMaison[self.monJour.value] = self.portefeuille[-1] / 100
             self.suiviMaison2[self.monJour.value] = self.portefeuille[0] / 100
-
-        pEvenementExt.terminate()
+        
+        #fin de la simulation pour Marche
+        pEvenementExt.terminate() #on ferme le process fils evenement ext
+        #on ferme les Queues
         self.tabMarche.close()
         self.tabMarche.join_thread()
         self.tabProduction.close()
         self.tabProduction.join_thread()
-
+    
+    #__test__: retourne vrai si toutes le flags du tableau partagee flagFinishTurn sont True
     def __test__(self, n):
         for i in range(n):
-            # print("Marche",self.flagFinishTurn[:],"jour",self.monJour.value)
             if self.flagFinishTurn[i] == False:
                 return False
         return True
 
     # Thread qui fait le lien avec les maisons
     def __achat_marche__(self, i, valeur):
-        # possible no need for lock : its a shared array memory (already implemented?
+        #maj du portefeuille de la maison i: valeur peut etre positif (vend) ou negatif(achete)
         self.portefeuille[i] += valeur * (self.vPrix[self.monJour.value] + 3.5)
 
-        # print(ancienneVal - valeur * self.vPrix[self.monJour.value],"ce qu on a fait",self.portefeuille[i])
-
-        # print("portefeuille",i,"montant",self.portefeuille[i])
-
+    #prix en fonction de l offre et de la demande en energie (ensemble des echanges d energie avec marche en une journee)
     def prix(val):
-
+        
         if val < 0:
             return (0.04)
 
         else:
             return (-0.03)
 
-        """a= - 0.001
-        for i in range(0,100000,20):
-            if val < i+20 and val >= i:
-                return(a)
-            a= a - 0.001
-        b = 0.05
-        for i in range(-100000,0,20):
-            if val > i and val <= i+20:
-                return(b)
-            b=b - 0.001"""
-
-
+#Process Maison
 class Maison(Process):
 
-    # __init__: constructeur, initialise tout les parametres
+    # __init__: initialise tout les parametres
     def __init__(self, numMaison, tabProduction, tabMarche, listTemp, flagFinishTurn, monJour,
                  synCondition, etatSimulation, lockProduction):
-        # penser a mettre le super sinon ca marche pas
         super().__init__()
-        # valeurs arbitraires
         self.consommation = 100
         self.production = 0
-        # on recupere le numero du process maison en cours
+        #numMaison: numero du process maison
         self.numMaison = numMaison
-        # queue tabProduction
         self.tabProduction = tabProduction
-
         self.listTemp = listTemp
-        # Portefeuilles maisons
         self.portefeuille = portefeuille
         self.tabMarche = tabMarche
-
         self.monJour = monJour
         self.synCondition = synCondition
         self.flagFinishTurn = flagFinishTurn
-        self.autorisationAchatMaison = 0
         self.etatSimulation = etatSimulation
         self.lockProduction = lockProduction
 
-    # run definie ce que fait le process
+    # run: definie ce que fait le process
     def run(self):
-        # print("creation maison",self.numMaison)
-        # condition pour changement de jour quand marche a maj monJour
-        # n=0
-        # print("premier jour",self.monJour.value)
+        
+        # boucle while pour que la simulation fonctionne sur 366 jours
         while self.monJour.value != 365:
-            # print("Maison",self.flagFinishTurn[:],"num", self.numMaison)
-            # n+=1
-            # print(self.numMaison,"refait une boucle nombre",n)
-            # self.flagFinishTurn[self.numMaison] = False
-            # on attend que tout les process maisons soient synch
-            # creation d'un thread (un thread par maison) qui va produire
+            # creation d'un thread qui va generee une valeur de production et une valeur de consommation
             threadProduire = threading.Thread(target=Maison.__produire__, args=(self,))
             threadProduire.start()
             threadProduire.join()
 
-            if self.etatSimulation == 0:  # on entre dans le mode de gestion communiste
+            if self.etatSimulation == 0:  # on entre dans le mode de gestion communiste(0)
                 if self.production > self.consommation:
+                    #on produit plus que ce que l'on consomme: on donne dans tabProduction notre surplus
                     threadDonnerMaison = threading.Thread(target=Maison.__donnerMaison__, args=(self,))
                     threadDonnerMaison.start()
                     threadDonnerMaison.join()
                 else:
-
+                    #on a consomme plus que ce que l'on a produit
+                    #on recupere aupres de la cagnotte dans tabProduction ou on achete au marche
                     threadAcheter = threading.Thread(target=Maison.__acheterCommuniste__, args=(self,))
                     threadAcheter.start()
                     threadAcheter.join()
 
-            else:  # on entre dans le mode de gestion capitaliste
+            else:  # on entre dans le mode de gestion capitaliste(1)
                 threadEchangeMarche = threading.Thread(target=Maison.__echangeMarche__, args=(self,))
                 threadEchangeMarche.start()
                 threadEchangeMarche.join()
 
-            # avertie qu'il a fini son jour
-            self.flagFinishTurn[self.numMaison] = True
-            self.synCondition.acquire()
-            self.synCondition.wait()
-            self.synCondition.release()
+            # fin des actions du jour pour ce process maison
+            self.flagFinishTurn[self.numMaison] = True #avertie a marche qu'il a finit son jour
+            self.synCondition.acquire() #comme un semaphore (pas de limite de taille)
+            self.synCondition.wait() #se met en attente de self.synCondition.notify()
+            self.synCondition.release() #libere son verroue de semaphore
 
     # thread produire de l'energie
     def __produire__(self):
         taux = abs(self.listTemp[self.monJour.value] - 20) / 20  # Nrj max pour 20°
         coefproduction = random.randint(10, 80)
         coefconso = random.randint(10, 80)
-
         self.production = (1 - taux) * 100 + coefproduction
         self.consommation = taux * 100 + coefconso
-        # print("maison",self.numMaison,"produit",self.production,"consomme",self.consommation)
 
     def __donnerMaison__(self):
-
+        #Mutex lockProduction (but): eviter le cas ou un process recupere la valeur de la cagnotte
+        #et avant de remettre cette valeur, un autre la voyant vide y ajoute son surplus
         with self.lockProduction:
             if self.tabProduction.empty() == False:
+                #on prend la valeur dans tabProduction et la stocke dans variable cagnotteProdMaisons
                 cagnotteProdMaisons = self.tabProduction.get()
-                # print("cagnotteProdMaisons",cagnotteProdMaisons)
                 cagnotteProdMaisons += (self.production - self.consommation)
+                #on met dans tabProduction la valeur modifiee avec le surplus
                 self.tabProduction.put(cagnotteProdMaisons)
-
+               
+                #ecriture dans fichier nrj.txt
                 fichier = open("nrj.txt", "a")
                 fichier.write(
                     "Maison " + str(self.numMaison) + " donne " + str(self.production - self.consommation) + " kW" + "\n")
                 fichier.close()
 
             else:
+                #on met directement dans tabProduction le surplus de production
                 self.tabProduction.put(self.production - self.consommation)
-
+                
+                #ecriture dans fichier nrj.txt
                 fichier = open("nrj.txt", "a")
                 fichier.write(
                     "Maison " + str(self.numMaison) + " donne " + str(self.production - self.consommation) + " kW" + "\n")
@@ -240,13 +224,12 @@ class Maison(Process):
 
     # thread achete de l'energie sur le marche ou entre les maisons disponibles
     def __acheterCommuniste__(self):
-
-        # print(self.numMaison,self.production,self.consommation)
-
-        if self.tabProduction.empty() == False:
+        
+        if self.tabProduction.empty() == False: #on peut prendre dans la cagnotte commune tabProduction
             with self.lockProduction:
                 cagnotteProdMaisons = self.tabProduction.get()
                 if cagnotteProdMaisons >= (self.consommation - self.production):
+                    #cas ou la cagnotte contient plus que ce dont on a besoin
                     cagnotteProdMaisons -= (self.consommation - self.production)
                     self.tabProduction.put(cagnotteProdMaisons)
 
@@ -254,10 +237,11 @@ class Maison(Process):
                     fichier.write("Maison " + str(self.numMaison) + " achète " + str(
                         self.production - self.consommation) + " kW" + "\n")
                     fichier.close()
-
+                    #maj de production pour eviter de rentrer dans la prochaine instruction if
                     self.production = self.consommation
 
                 else:
+                    #cas ou la cagnotte contient moins que ce dont on a besoin: on prend tout ce qu'il y a et maj de production
                     fichier = open("nrj.txt", "a")
                     fichier.write("Maison " + str(self.numMaison) + " achète " + str(cagnotteProdMaisons) + " kW" + "\n")
                     fichier.close()
@@ -265,16 +249,19 @@ class Maison(Process):
                     self.production += cagnotteProdMaisons
                     self.tabProduction.put(0)
 
-        if self.production < self.consommation:
+        if self.production < self.consommation:#achat au marche
+            #tabProduction etait vide ou la cagnotte n a pas suffit pour combler le deficit
+            
             fichier = open("nrj.txt", "a")
             fichier.write("Maison " + str(self.numMaison) + " achète au marché " + str(
                 self.production - self.consommation) + " kW" + "\n")
             fichier.close()
-
+            
+            #on envoie dans tabMarche (queue) a destination de marche notre id (numMaison) et l energie qu il nous faut
             tupleSend = (self.numMaison, self.production - self.consommation)
             self.tabMarche.put(tupleSend)
 
-            # print("apres marche","produit",self.production,"consomme",self.consommation)
+            
 
     def __echangeMarche__(self):
         # marche met a jour le portefeuille
@@ -282,14 +269,13 @@ class Maison(Process):
         fichier.write("Maison " + str(self.numMaison) + " échange au marché " + str(
             self.production - self.consommation) + " kW" + "\n")
         fichier.close()
-
+        #si prod>conso on va ganger de l arget sinon la difference est negative=on perdra de l argent
         tupleSend = (self.numMaison, self.production - self.consommation)
         self.tabMarche.put(tupleSend)
 
-
+#Process Fils evenement exterieur (communique par pipes avec Marche)
 def evenementExt(child_conn):
-    # print("je suis un evenementExt")
-
+    #monJour: valeur du jour synchronise avec Marche
     monJour = child_conn.recv();
     while monJour != 365:
         # generation aleatoire d un bonus ou d un malus
@@ -305,54 +291,66 @@ def evenementExt(child_conn):
         monJour = child_conn.recv();
 
 
+#main programm
 if __name__ == "__main__":
 
     print(" !! Ne pas oublier de créer les fichiers textes : Courbes.txt nrj.txt")
     print(" ### ------------ SIMULATION --------------- ###")
-
+    
+     # etatSimulation : 0 vaut communiste 1 vaut capitaliste
     etatSimulation = int(input(
-        "Quel mode de simulation voulez vous faire ? (0 : communiste, 1: capitaliste) "))  # 0 vaut communiste 1 vaut capitaliste
+        "Quel mode de simulation voulez vous faire ? (0 : communiste, 1: capitaliste) ")) 
     N = 366  # Nombre de jours de la simulation
     listTemp = Array('d', range(N))  # Liste des températures
     nMaison = int(input("Quel nombre de maisons voulez vous pour la simulation ? "))  # nombre de maisons
-    suiviMaison = Array('d', range(N))
+    suiviMaison = Array('d', range(N)) 
     suiviMaison2 = Array('d', range(N))
 
-    # Méteo (process qui genere 300 temp dans memoire partagee listTemp
+    # Méteo (process qui genere 300 temp dans la memoire partagee listTemp)
     pMeteo = Process(target=meteo, args=(listTemp, N))
     pMeteo.start()
-
-    monJour = Value('i', 0)
-    synCondition = Condition()
-
-    vPrix = Array('d', range(N))  # prix gere par marche
+    pMeteo.join()
+    
+    monJour = Value('i', 0) #variable partagee entre Marche et Maison et qui donne le jour en cours
+    
+    synCondition = Condition() #semaphore particulier pour synchroniser les maisons avec le marche
+    
+    vPrix = Array('d', range(N))  #tableau partage: prix gere et mis a jour par marche
     vPrix[0] = 0.8  # prix initial
-    flagFinishTurn = Array('i', [
-        False] * nMaison)  # se met a True chaque jour quand maison a finie ses echanges pour prevenir marche
-
+    
+    # flagFinishTurn: drapeau de fin de jour =se met a True chaque jour quand maison a finie ses echanges pour prevenir marche
+    flagFinishTurn = Array('i', [False] * nMaison)  
+    
+    #tabProduction: Queue partagee entre les maisons et qui gere les echanges de surplus de prod entre les maisons (cagnotte commune)
     tabProduction = Queue()
+    #lockProduction: Mutex pour gerer l'acces partage a tabProduction entre les maisons
     lockProduction = Lock()
+    #tabMarche: Queue partagee entre les maisons et marches pour gere les echanges d energie
     tabMarche = Queue()
-    portefeuille = Array('d', [0] * nMaison)  # creation d'un portefeuille pour les 10 maisons process
+    #portefeuille:Tableau partage gere par Marche dans notre simulation 
+    #(chaque maison a un portefeuille maj en fonction des echanges avec Marche)
+    portefeuille = Array('d', [0] * nMaison)  
 
     # On lance le marché
     pMarche = Marche(vPrix, tabMarche, listTemp, portefeuille, flagFinishTurn, monJour, synCondition, suiviMaison,
                      tabProduction, nMaison, suiviMaison2)
     pMarche.start()
+    
+    
     # On lance les process maisons
-
-    pMaison = []
+    pMaison = [] #tableau de process :utilise a la fin de la simulation pour terminer les process maisons 
     for numMaison in range(nMaison):
         maison = Maison(numMaison, tabProduction, tabMarche, listTemp, flagFinishTurn, monJour, synCondition,
                         etatSimulation, lockProduction)
         maison.start()
         pMaison.append(maison)
 
+    #fin de la simulation: on ferme les process pMarche et maison
     pMarche.join()
     for j in pMaison:
         j.terminate()
 
-    pMeteo.join()
+   
 
     P = []
 
